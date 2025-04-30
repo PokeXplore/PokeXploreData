7cbb6de063377c2a3055841323a6d736d115b3c8
Flarkinater <mcteaguealex@gmail.com>
2025-04-30 13:23:34 -0400

    Add gym badge items

--- src/main/java/com/github/flarkinater/pokexplore/item/PokeXploreItems.java
+++ src/main/java/com/github/flarkinater/pokexplore/item/PokeXploreItems.java
@@ -21,6 +21,25 @@ public class PokeXploreItems {
                 .register((itemGroup) -> itemGroup.add(PokeXploreItems.MUSIC_DISC_CHALICE));
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS)
                 .register((itemGroup) -> itemGroup.add(PokeXploreItems.MUSIC_DISC_THE_DRILL));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+                .register((itemGroup) -> itemGroup.add(PokeXploreItems.BADGE_BASILISK));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+                .register((itemGroup) -> itemGroup.add(PokeXploreItems.BADGE_BIOLUMINESCENCE));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+                .register((itemGroup) -> itemGroup.add(PokeXploreItems.BADGE_CHILLING));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+                .register((itemGroup) -> itemGroup.add(PokeXploreItems.BADGE_DREADNOUGHT));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+                .register((itemGroup) -> itemGroup.add(PokeXploreItems.BADGE_DUALITY));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+                .register((itemGroup) -> itemGroup.add(PokeXploreItems.BADGE_FABLE));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+                .register((itemGroup) -> itemGroup.add(PokeXploreItems.BADGE_INFERNAL));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+                .register((itemGroup) -> itemGroup.add(PokeXploreItems.BADGE_METEOR));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+                .register((itemGroup) -> itemGroup.add(PokeXploreItems.BADGE_TERRABYTE));
+
     }
 
     public static Item register(Item item, String id) {
@@ -49,5 +68,41 @@ public class PokeXploreItems {
     public static final Item MUSIC_DISC_THE_DRILL = register(
             new Item(new Item.Settings().jukeboxPlayable(PokeXploreSounds.THE_DRILL_KEY).maxCount(1)),
             "music_disc_the_drill"
+    );
+    public static final Item BADGE_BASILISK = register(
+            new Item(new Item.Settings()),
+            "badge_basilisk"
+    );
+    public static final Item BADGE_BIOLUMINESCENCE = register(
+            new Item(new Item.Settings()),
+            "badge_bioluminescence"
+    );
+    public static final Item BADGE_CHILLING = register(
+            new Item(new Item.Settings()),
+            "badge_chilling"
+    );
+    public static final Item BADGE_DREADNOUGHT = register(
+            new Item(new Item.Settings()),
+            "badge_dreadnought"
+    );
+    public static final Item BADGE_DUALITY = register(
+            new Item(new Item.Settings()),
+            "badge_duality"
+    );
+    public static final Item BADGE_FABLE = register(
+            new Item(new Item.Settings()),
+            "badge_fable"
+    );
+    public static final Item BADGE_INFERNAL = register(
+            new Item(new Item.Settings()),
+            "badge_infernal"
+    );
+    public static final Item BADGE_METEOR = register(
+            new Item(new Item.Settings()),
+            "badge_meteor"
+    );
+    public static final Item BADGE_TERRABYTE = register(
+            new Item(new Item.Settings()),
+            "badge_terrabyte"
     );
 }
